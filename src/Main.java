0d1b4270ef315126db4b3cd054a49904c8a88edc
ahyh60 <123133761+ahyh60@users.noreply.github.com>
2023-11-09 14:13:10 +0800

    Update Main.java

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,34 @@
 public class Main {
     public static void main(String[] args) {
         System.out.println("Hello world!");
+        int arr[] = { 2, 3, 4, 10, 40 };
+
+    }
+
+
+    public void selectionSort(int[] array){
+        for(int i = 0; i < array.length-1; i++ ){
+            int minIndex = i;
+            for(int j = i+1; j < array.length; j++ ){
+                if(array[minIndex] > array[j])
+                    minIndex = j;
+            }
+            int temp = array[i];
+            array[i] = array[minIndex];
+            array[minIndex] = temp;
+        }
+
+    }
+
+    public void bubbleSort(int[] array){
+       for(int i = 0; i < array.length-1; i++){
+           for(int j = 0; j < array.length-1; j++){
+               if(array[j] > array[j+1]){
+                   int temp = array[j];
+                   array[j] = array[j+1];
+                   array[j+1] = temp;
+               }
+           }
+       }
     }
 }
